c6a5882d07508a873fd61ebff5cfa86afecc747a
tkusal <thiago.kusal@outlook.com>
2025-10-14 22:27:30 -0300

    Aula 14/10/2025

---
+++ Semana 2/Ex3 - PARA CASA.c
@@ -0,0 +1,9 @@
+/*
+    Escreva um programa que receba a idade de uma pessoa.
+
+    Se essa pessoa tiver mais de 18 anos, ela é adulto.
+    Se a idade dela for entre 12 e 18, é adolescente.
+    Se ela tiver menos de 12 anos, é criança.
+
+    O programa deverá escrever na tela, a "qualificação" dessa pessoa de acordo com a idade.
+*/
---
+++ Semana 2/Ex4 - PARA CASA.c
@@ -0,0 +1,11 @@
+/*
+    Escreva um programa que receba a altura e peso de uma pessoa.
+    O programa deverá calcular o IMC dessa pessoa e informar se ela está: 
+
+    Abaixo do peso - IMC menor ou igual 18,5
+    Peso normal - IMC entre 18,5 e 24,99
+    Sobrepeso - IMC entre 25 e 29,99
+    Obeso - IMC maior ou igual a 30
+
+    IMC = PESO / ALTURA²
+*/
