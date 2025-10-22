7202581ef58ca6f60c061bddadf1d3fd4a8900ca
tkusal <thiago.kusal@outlook.com>
2025-10-21 22:17:59 -0300

    Aula 21/10/2025

--- Semana 3/Ex3 - PARA CASA.c
+++ Semana 3/Ex3 - PARA CASA.c
@@ -7,3 +7,17 @@
     ....
     9x9 = 81
 */
+
+#include <stdio.h>
+
+int main() {
+
+    for (int i = 1; i <= 9; i++) {
+        for (int j = 1; j <= 10; j++ ) {
+            printf("%d x %d = %d \n", i, j, i*j);
+        }
+        printf("------------------\n");
+    }
+
+    return 0;
+}
